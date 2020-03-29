f7934339ef34806f28759a0c21111e9bb0d2e9f2
Joshua Uduehi <33010247+jUdu97@users.noreply.github.com>
2020-03-29 16:07:09 -0500

    adjusted video
    changed source for video

--- src/home.js
+++ src/home.js
@@ -13,9 +13,6 @@ class Homepage extends Component {
             This section serves as an introduction to the marketplace, the
             variety from which to choose from, and what we're all about.
           </h2>
-          <Player>
-            <source src="https://vod-progressive.akamaized.net/exp=1585505326~acl=%2A%2F1208946501.mp4%2A~hmac=de7d52e4387430391290caf29adf0ef57e31f444966defad03c563a2b7274af0/vimeo-prod-skyfire-std-us/01/2698/12/313490233/1208946501.mp4" />
-          </Player>
         </div>
       </div>
     );
