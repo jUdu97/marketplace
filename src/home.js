c7caa24d26b321199251c05fd8f7031d30e75c3c
Joshua Uduehi <33010247+jUdu97@users.noreply.github.com>
2020-03-29 16:11:30 -0500

    deleted video
    readjusted video component

--- src/home.js
+++ src/home.js
@@ -13,6 +13,9 @@ class Homepage extends Component {
             This section serves as an introduction to the marketplace, the
             variety from which to choose from, and what we're all about.
           </h2>
+          <Player>
+            <source src="https://vod-progressive.akamaized.net/exp=1585529817~acl=%2A%2F1208946501.mp4%2A~hmac=aa065062b4164e613c4b87f14d697a685ade8fad595fa4807a1351fbc40b2942/vimeo-prod-skyfire-std-us/01/2698/12/313490233/1208946501.mp4" />
+          </Player>
         </div>
       </div>
     );
